2a2277d42d1cab9b632fa0100e1d33337d3c3887
Moazzama khatri <159924224+KhatriMoazzama@users.noreply.github.com>
2024-07-11 19:36:29 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer  from "inquirer";
+import chalk from "chalk";
+
+let user_message = await inquirer.prompt(
+    [
+        {
+            name : 'user_text',
+            message : "Enter message here : ",
+            type : 'editor',
+        }
+    ]
+)
+
+let message : string[] = user_message.user_text
+console.log(message)
+let character_counter = 0
+for(let i =0;i<= message.length; i++){
+    if(message[i]== " ")
+    {
+        i++;
+    }
+    else
+    {
+        character_counter+=1
+    }
+}
+console.log("total character in this text is ",character_counter)
